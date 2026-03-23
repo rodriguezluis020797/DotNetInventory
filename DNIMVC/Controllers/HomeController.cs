77f41f06d22e69808fbd4605e81f70021eeae2cd
Luis Rodriguez <rodriguez.luis020797@gmail.com>
2026-03-23 17:15:19 -0600

    initial project creation

---
+++ DNIMVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using DNIMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DNIMVC.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
